0887bb7f3092d54bd626e5113fa65a08645b1f9b
zhangheng <zhangheng@allinmd.cn>
2019-07-25 09:07:48 +0800

    项目：心心相遇H5相遇 描述：单模块启动 author：zhangheng

---
+++ src/components/TabBar.vue
@@ -0,0 +1,24 @@
+<template>
+    <section class="ml-components-tabBar">
+        <div class="ml-components-tabBar-item home active">
+            <i class="icon"></i>
+            <span class="title">首页</span>
+        </div>
+        <div class="ml-components-tabBar-item message">
+            <i class="icon"></i>
+            <span class="title">消息</span>
+        </div>
+        <div class="ml-components-tabBar-item personal">
+            <i class="icon"></i>
+            <span class="title">我的</span>
+        </div>
+    </section>
+</template>
+<script>
+export default {
+
+};
+</script>
+<style scoped lang="scss">
+    @import "@styles/components/tabBar.scss";
+</style>
---
+++ src/pages/index/index.vue
@@ -0,0 +1,33 @@
+<template>
+    <section class="ml-mainInner">
+        <Tab></Tab>
+    </section>
+</template>
+
+<script type="text/ecmascript-6">
+import Tab from '@components/TabBar.vue';
+export default {
+  components: {
+    Tab
+  },
+  data() {
+    return {
+
+    };
+  },
+  beforeMount() {
+  },
+  computed: {
+
+  },
+  methods: {
+
+  },
+  watch: {
+
+  }
+};
+</script>
+<style lang="scss" rel="stylesheet/scss">
+    @import "@styles/pages/index/index.scss";
+</style>
