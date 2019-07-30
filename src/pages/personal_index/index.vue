ae0ca089b677c8db66b08feb6ec9758aa08ba8b7
jhony <zxf9628@163.com>
2019-07-30 23:06:21 +0800

    更新了personal_index/index.vue，personal_index/personal_index.scss，完善未登录ui

--- src/pages/personal_index/index.vue
+++ src/pages/personal_index/index.vue
@@ -2,11 +2,38 @@
     <section class="ml-mainInner">
         <HeaderModule :config="headerConfig"></HeaderModule>
         <div class="loginBox">
-            <div class="login_top">
-                <span class="login_icon_set"></span>
-                <span class="login_icon_option"></span>
-                <img src="" alt="" class="login_userImg">
-                <span class="login_btn">立即登录</span>
+            <div class="loginBox_header">
+                <div class="login_top">
+                    <span class="login_icon_set"></span>
+                    <span class="login_icon_option"></span>
+                    <img src="" alt="" class="login_userImg">
+                    <span class="login_btn">立即登录</span>
+                </div>
+                <div class="login_main">
+                    <div class="login_main_item">
+                        <span class="main_userInfo_icon"></span>
+                        <span class="main_userInfo_text">基本信息</span>
+                    </div>
+                    <div class="login_main_item">
+                        <span class="main_userInfo_icon"></span>
+                        <span class="main_userInfo_text">审核信息</span>
+                    </div>
+                    <div class="login_main_item">
+                        <span class="main_userInfo_icon"></span>
+                        <span class="main_userInfo_text">个人发布</span>
+                    </div>
+                    <div class="login_main_item">
+                        <span class="main_userInfo_icon"></span>
+                        <span class="main_userInfo_text">我的留言</span>
+                    </div>
+                </div>
+            </div>
+            <div class="loginBox_footer">
+                <div class="noLogin_img"></div>
+                <div class="login_personalFlag">
+                    <span class="login_personalFlag_text">拒绝轻浮与盲目，只为遇见我的Ta~</span>
+                </div>
+                <div class="loginBox_footer_btn">立即登录</div>
             </div>
         </div>
         <Tab :nav="navIndex"></Tab>
