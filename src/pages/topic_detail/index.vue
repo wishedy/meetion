04223406f8a84393253e51dfac7e056eb852bc35
张恒 <zhfirefly@yahoo.com>
2019-07-30 21:48:27 +0800

    tab栏icon添加

---
+++ src/pages/topic_detail/index.vue
@@ -0,0 +1,44 @@
+<template>
+    <section class="ml-mainInner">
+        <section class="ml-topic-detail">
+            <TopicBanner></TopicBanner>
+            <TopicContent></TopicContent>
+            <Dynamic></Dynamic>
+            <DiscussTopic></DiscussTopic>
+        </section>
+    </section>
+</template>
+
+<script type="text/ecmascript-6">
+import TopicBanner from '@components/topicBanner.vue';
+import TopicContent from '@components/topicContent.vue';
+import Dynamic from '@components/Dynamic.vue';
+import DiscussTopic from '@components/DiscussTopic.vue';
+export default {
+  components: {
+    TopicBanner,
+    TopicContent,
+    Dynamic,
+    DiscussTopic
+  },
+  data() {
+    return {
+
+    };
+  },
+  beforeMount() {
+  },
+  computed: {
+
+  },
+  methods: {
+
+  },
+  watch: {
+
+  }
+};
+</script>
+<style lang="scss" rel="stylesheet/scss">
+    @import "@styles/pages/topic_dynamic_detail/topic_detail.scss";
+</style>
---
+++ src/pages/topic_dynamic_detail/index.vue
@@ -0,0 +1,65 @@
+<template>
+    <section class="ml-mainInner">
+        <HeaderModule :config="headerConfig"></HeaderModule>
+        <section class="ml-topicDynamic-detail">
+            <section class="ml-topicBanner-container">
+                <section class="banner">
+                    <section class="topic-logo">
+                        <div class="mask">
+                            <i class="icon"></i>
+                        </div>
+                    </section>
+                    <section class="ml-topic-info">
+                        <h1 class="title">
+                            <span class="sign">T：</span>
+                            <em>你的城市下雪了吗？</em>
+                        </h1>
+                        <section class="ml-topic-handle">
+                            <div class="ml-take-partIn">加入讨论</div>
+                            <div class="ml-take-all">查看所有</div>
+                        </section>
+                    </section>
+                </section>
+            </section>
+            <Dynamic></Dynamic>
+        </section>
+        <PublishBar></PublishBar>
+    </section>
+</template>
+
+<script type="text/ecmascript-6">
+import HeaderModule from '@components/HeaderModule.vue';
+import Tab from '@components/TabBar.vue';
+import PublishBar from '@components/PublishBar.vue';
+import Dynamic from '@components/Dynamic.vue';
+export default {
+  components: {
+    HeaderModule,
+    Tab,
+    PublishBar,
+    Dynamic
+  },
+  data() {
+    return {
+      headerConfig: {
+        backOnOff: true,
+        title: '话题'
+      }
+    };
+  },
+  beforeMount() {
+  },
+  computed: {
+
+  },
+  methods: {
+
+  },
+  watch: {
+
+  }
+};
+</script>
+<style lang="scss" rel="stylesheet/scss">
+    @import "@styles/pages/topic_dynamic_detail/topic_dynamic_detail.scss";
+</style>
