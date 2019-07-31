67b2214013a18302bdf4b8e637c6346ff58a5242
jhony <zxf9628@163.com>
2019-07-31 22:42:28 +0800

    更新登录模块，提交信息部分

--- src/pages/personal_index/index.vue
+++ src/pages/personal_index/index.vue
@@ -1,6 +1,6 @@
 <template>
     <section class="ml-mainInner">
-        <HeaderModule :config="headerConfig"></HeaderModule>
+        <!--<HeaderModule :config="headerConfig"></HeaderModule>-->
         <div class="loginBox">
             <div class="loginBox_header">
                 <div class="login_top">
@@ -37,6 +37,41 @@
             </div>
         </div>
         <Tab :nav="navIndex"></Tab>
+        <div class="login_tip">
+            <div class="loginTip_title">
+                <span class="loginTip_iconLeft"></span>
+                <span class="loginTip_title_text">欢迎登录</span>
+                <span class="loginTip_iconR"></span>
+                <span class="loginTip_close"></span>
+            </div>
+            <div class="loginTip_img"></div>
+            <div class="loginInfo">
+                <input type="number" class="loginInfo_input" placeholder="请输入您的账号" max="11" />
+            </div>
+            <div class="loginInfo">
+                <input type="number" class="loginInfo_input" placeholder="请输入您的密码" max="11" />
+            </div>
+            <div class="loginInfo_style">
+                <span class="loginInfo_styleTip">手机号登录</span>
+                <span class="loginInfo_styleTip2">忘记密码？</span>
+            </div>
+            <div class="loginInfo_btn">开启遇见</div>
+            <div class="loginOther">
+                <p class="loginOther_register">快速注册</p>
+                <div class="loginOther_three">
+                    <span class="loginOther_line"></span>
+                    <span class="loginOther_loginThree">第三方</span>
+                    <span class="loginOther_line"></span>
+                </div>
+                <div class="loginOther_wx">
+                    <span class="loginOther_wxImg"></span>
+                </div>
+                <div class="loginOther_userRead">
+                    <span class="loginOther_readText">登录表示您同意 </span>
+                    <span class="loginOther_readNote">《用户协议》</span>
+                </div>
+            </div>
+        </div>
     </section>
 </template>
 
