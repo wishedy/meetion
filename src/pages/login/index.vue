f35683cc26b7dfae776d57a7e253bb102d75f349
张恒 <zhfirefly@yahoo.com>
2019-08-04 01:53:23 +0800

    tab栏icon添加

---
+++ src/pages/login/index.vue
@@ -0,0 +1,54 @@
+<template>
+    <section class="ml-mainInner">
+        <section class="ml-login-title">
+            <h1 class="title">欢迎登陆</h1>
+            <div class="close-btn"></div>
+        </section>
+        <section class="ml-slogan-container">
+            <Slogan></Slogan>
+        </section>
+        <section class="ml-form-phone">
+            <InputPhone></InputPhone>
+            <VerificationCode></VerificationCode>
+        </section>
+        <section class="ml-open-metion">开启遇见</section>
+        <section class="ml-authorization">
+            <article class="register">快速注册</article>
+            <p class="third-line">第三方</p>
+            <figure class="third-logo"></figure>
+            <section class="userAgreement">登录表示您同意 <em>《用户协议》</em></section>
+        </section>
+    </section>
+</template>
+
+<script type="text/ecmascript-6">
+import Slogan from '@components/Slogan';
+import InputPhone from './components/InputPhone.vue';
+import VerificationCode from './components/VerificationCode.vue';
+export default {
+  components: {
+    Slogan,
+    InputPhone,
+    VerificationCode
+  },
+  data() {
+    return {
+
+    };
+  },
+  beforeMount() {
+  },
+  computed: {
+
+  },
+  methods: {
+
+  },
+  watch: {
+
+  }
+};
+</script>
+<style lang="scss" rel="stylesheet/scss">
+    @import "@styles/pages/login/login.scss";
+</style>
---
+++ src/pages/set_center/index.vue
@@ -0,0 +1,47 @@
+<template>
+    <section class="ml-mainInner">
+        <HeaderModule :config="headerConfig"></HeaderModule>
+        <section class="ml-set-center">
+            <LinkItem txt="关于我们"></LinkItem>
+            <LinkItem txt="修改密码"></LinkItem>
+            <LinkItem txt="意见反馈"></LinkItem>
+            <LinkItem txt="我要举报"></LinkItem>
+        </section>
+        <SureBtn txt="退出登录"></SureBtn>
+    </section>
+</template>
+
+<script type="text/ecmascript-6">
+import LinkItem from '@components/LinkItem.vue';
+import SureBtn from '@components/SureBtn.vue';
+import HeaderModule from '@components/HeaderModule.vue';
+export default {
+  components: {
+    LinkItem,
+    HeaderModule,
+    SureBtn
+  },
+  data() {
+    return {
+      headerConfig: {
+        backOnOff: true,
+        title: '设置'
+      }
+    };
+  },
+  beforeMount() {
+  },
+  computed: {
+
+  },
+  methods: {
+
+  },
+  watch: {
+
+  }
+};
+</script>
+<style lang="scss" rel="stylesheet/scss">
+    @import "@styles/pages/set_center/set_center.scss";
+</style>
